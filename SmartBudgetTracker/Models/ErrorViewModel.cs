423c32f40168df65b56ec20503dcf10d37592395
basseychrist <enobongetimbassey101gmail.com>
2026-02-05 11:30:16 +0100

    Refactor code structure for improved readability and maintainability. Setup Development ENV.

---
+++ SmartBudgetTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SmartBudgetTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
